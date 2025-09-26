1bd264d5eec9a7aa8ee3e33b8f146e91b0d29f15
anyaBrewer <ahbrew26@gmail.com>
2025-09-26 14:58:06 -0400

    Made audiobook and ebook

---
+++ src/AudioBook.java
@@ -0,0 +1,45 @@
+public class AudioBook extends Media {
+    private String author;
+    private int yearPublished;
+    private String narrator;
+
+    public AudioBook(String title, String author, int yearPublished, double price,
+                     String narrator) {
+        super(title, price);
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.narrator = narrator;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getNarrator() {
+        return narrator;
+    }
+
+    public void setNarrator(String narrator) {
+        this.narrator = narrator;
+    }
+
+    public void printDetails() {
+        System.out.println("Audio Book Title: " + getTitle());
+        System.out.println("Author: " + author);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + getPrice());
+        System.out.println("Narrator: " + narrator);
+    }
+}
---
+++ src/EBook.java
@@ -0,0 +1,35 @@
+public class EBook extends Media {
+    private String author;
+    private int yearPublished;
+
+    public EBook(String title, String author, int yearPublished, double price) {
+        super(title, price);
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("E-Book Title: " + getTitle());
+        System.out.println("Author: " + author);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + getPrice());
+    }
+
+}
