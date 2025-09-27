7beeebb3ab6e720e1c9be79c56b905b1b93f8040
Jack Reddy <jeredd27@colby.edu>
2025-09-27 18:09:17 -0400

    Added the Address Class with getters and constructor.

---
+++ src/Address.java
@@ -0,0 +1,37 @@
+class Adress{
+    private String Line1;
+    private String Line2;
+    private String City;
+    private String State;
+    private String Zip;
+    private String Country;
+
+    public Address(Line1, Line2, City, State, Zip, Country){
+    }
+
+    public String getLine1(){
+        return this.Line1;
+    }
+
+    public String getLine2(){
+        return this.Line2;
+    }
+
+    public String getCity(){
+        return this.City;
+    }
+
+    public String getState(){
+        return this.State;
+    }
+
+    public String getZip(){
+        return this.Zip;
+    }
+
+    public String getCountry(){
+        return this.Country;
+    }
+    
+
+}
