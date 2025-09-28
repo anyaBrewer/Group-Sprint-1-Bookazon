7986c95e673ec09038800f9ebecab77bf2396c0d
Jack Reddy <jeredd27@colby.edu>
2025-09-28 15:45:11 -0400

    Removed book class and reolaced it with HardcoverBook and PaperbackBook classes

---
+++ src/PaperbackBook.java
@@ -0,0 +1,49 @@
+public class PaperbackBook extends Media{
+    private String author;
+    private int yearPublished;
+
+    public PaperbackBook(String title, String author, int yearPublished, double price, boolean isPaperback) {
+        super(title, price);
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+
+    public void updateBookDetails(Book book, String newTitle, String newAuthor, int newYearPublished, double newPrice, boolean isPaperback) {
+       setTitle(newTitle);
+        setAuthor(newAuthor);
+        setYearPublished(newYearPublished);
+        setPrice(newPrice);
+    }
+
+    public void printDetails() {
+        System.out.println("Title: " + this.getTitle());
+        System.out.println("Author: " + author);
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + this.getPrice());
+    }
+
+    public boolean isAuthorValid() {
+        return author != null && !author.isEmpty();
+    }
+
+    public boolean isYearPublishedValid() {
+        return yearPublished > 0;
+    }
+}
