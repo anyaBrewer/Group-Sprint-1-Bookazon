539c9e2b8ddd8ae1f5117db5cdf146d2a00d987b
Jack Reddy <jeredd27@colby.edu>
2025-09-28 15:30:58 -0400

    Added type declerations in constructor in Address

--- src/Order.java
+++ src/Order.java
@@ -16,11 +16,11 @@ public class Order {
     }
 
     public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.shippingAddress = new Address(line1, line2, city, state, zip, country)
+        this.shippingAddress = new Address(line1, line2, city, state, zip, country);
     }
 
     public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.billingAddress = new Address(line1, line2, city, state, zip, country)
+        this.billingAddress = new Address(line1, line2, city, state, zip, country);
     }
 
     public void setOrderStatus(String status) {
--- src/User.java
+++ src/User.java
@@ -37,11 +37,11 @@ public class User {
     }
 
     public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.shippingAddress = new Address(line1, line2, city, state, zip, country)
+        this.shippingAddress = new Address(line1, line2, city, state, zip, country);
     }
 
     public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.billingAddress = new Address(line1, line2, city, state, zip, country)
+        this.billingAddress = new Address(line1, line2, city, state, zip, country);
     }
 
 
